346fa1a638c59be283e4b97ed3363f44e5908f3f
AayushiAmonkar <aayushiamonkar@gmail.com>
2022-06-04 17:19:34 +0530

    Done with intial styling

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import Square from './Square';
 const Board = () => {
   return (
-    <div>
-      <div>
+    <div className="board">
+      <div className="board-row">
         <Square value={0} />
         <Square value={1} />
         <Square value={2} />
+        {/* <Square value={9}>
+          <p>Aayushi</p>
+          <h2>Amonkar</h2>
+        </Square> */}
+        {/* u can get this as children in  in props */}
       </div>
-      <div>
+      <div className="board-row">
         <Square value={3} />
         <Square value={4} />
         <Square value={5} />
       </div>
-      <div>
+      <div className="board-row">
         <Square value={6} />
         <Square value={7} />
         <Square value={8} />
