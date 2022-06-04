cf8ca04919f75c62ba993dc99780decf70344e08
AayushiAmonkar <aayushiamonkar@gmail.com>
2022-06-04 18:10:40 +0530

    now play tic tac toe

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+  //   console.log(board);
+  const handleSquareClick = position => {
+    // console.log(board);
+    if (board[position]) {
+      return;
+    }
+    setBoard(prev => {
+      //   console.log(isXNext);
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+    setIsXNext(!isXNext);
+  };
+  const renderSquare = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => {
+          handleSquareClick(position);
+        }}
+      />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
-        {/* <Square value={9}>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+
+        {/* <Square value={board[0]} onClick={()=>{handleSquareClick(0)}} />
+        <Square value={board[1]} />
+    <Square value={board[2]} /> */}
+        {/* <Square value={0}}>
           <p>Aayushi</p>
           <h2>Amonkar</h2>
         </Square> */}
         {/* u can get this as children in  in props */}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {/* <Square value={board[3]} />
+        <Square value={board[4]} />
+    <Square value={board[5]} /> */}
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {/* <Square value={board[6]} />
+        <Square value={board[7]} />
+    <Square value={board[8]} /> */}
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import './Square.style.css';
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
